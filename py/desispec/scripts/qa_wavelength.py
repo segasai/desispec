92ffc17517091efcdbf62c063e17395a05fa5694
profxj <xavier@ucolick.org>
2018-01-02 17:23:00 -0800

    scripting

---
+++ py/desispec/scripts/qa_wavelength.py
@@ -0,0 +1,48 @@
+# Script for checking wavelength solutions
+from __future__ import absolute_import, division
+
+from desiutil.log import get_logger
+import argparse
+
+from desispec.qa import __offline_qa_version__
+
+def parse(options=None):
+    parser = argparse.ArgumentParser(description="Generate Wavelength QA [v{:s}]".format(__offline_qa_version__))
+    parser.add_argument('--expid', type = int, required=True, help='Exposure ID')
+    #parser.add_argument('--channels', type=str, help="List of channels to include. Default = b,r,z]")
+    parser.add_argument('--reduxdir', type = str, default = None, metavar = 'PATH',
+                        help = 'Override default path ($DESI_SPECTRO_REDUX/$SPECPROD) to processed data.')
+    parser.add_argument('--sky', default = False, action='store_true',
+                        help = 'Generate plot showing offset from archived sky?')
+
+
+    args = None
+    if options is None:
+        args = parser.parse_args()
+    else:
+        args = parser.parse_args(options)
+    return args
+
+
+def main(args) :
+
+    from desispec.io import meta
+    from desispec.qa.qa_plots import exposure_sky_wave
+    log=get_logger()
+
+    log.info("starting")
+    if args.reduxdir is None:
+        specprod_dir = meta.specprod_root()
+    else:
+        specprod_dir = args.reduxdir
+    #if args.channels is None:
+    #    channels = ['b','r','z']
+    #else:
+    #    channels = [iarg for iarg in args.channels.split(',')]
+
+    # Fiber QA
+    if args.sky:
+        exposure_sky_wave(args.expid)
+
+
+
