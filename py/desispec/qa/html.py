456e3efc9a476ddd48f74d7700d84e8217a39ee7
profxj <xavier@ucolick.org>
2019-06-24 10:37:58 -0700

    check in NB

--- py/desispec/qa/html.py
+++ py/desispec/qa/html.py
@@ -16,7 +16,6 @@ def header(title):
 
     Returns
     -------
-
     """
     head = '<?xml version="1.0" encoding="UTF-8"?>\n'
     head += '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">\n'
